522e25b8bd158ca34c187f4361877f6f95a145c5
TomiSalo <tomi.a.salo@outlook.com>
2020-03-29 20:05:06 +0300

    Toiminta valmis

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input type="text" v-model="title" name="title" placeholder="Add todo...">
+            <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addTodo(e) {
+            e.preventDefault();
+            const newTodo = {
+                title: this.title,
+                completed: false
+            }
+            //Send to parent
+            this.$emit('add-todo', newTodo);
+            console.log(123)
+            this.title = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input[type="text"] {
+        padding: 10px;
+        flex: 10;
+    }
+    input[type="submit"] {
+        flex: 1;
+        background: #444;
+        color: #fff;
+        border: none;
+    }
+</style>
